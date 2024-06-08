ad7d7db513a71722a397a020d49a256449cdb35a
mamietti <mauri.miettinen13@gmail.com>
2024-06-08 17:16:08 +0300

    Remove lua and other GMod-specific things

--- lua/entities/livable_module/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type 		= "anim"
-ENT.Base 		= "base_sb_environment"
-ENT.PrintName 	= "SBEP Habitable module"
-
-ENT.Spawnable = false
-ENT.AdminSpawnable = false
-
-list.Set( "LSEntOverlayText" , "livable_module", {HasOOO = true, resnames ={ "oxygen", "energy", "water", "nitrogen"}} )
